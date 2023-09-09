a57633511cc5b22b47ef342b2a15cb5013e15f1f
gclarke000412 <gc000412@gmail.com>
2023-09-08 17:31:10 -0700

    revised-commit

--- src/config/auth0.config.ts
+++ src/config/auth0.config.ts
@@ -1,4 +1,4 @@
 export const auth0Config = {
     domain: "dev-egl67m5ddxa2b144.us.auth0.com",
-    clientId: "ee7ZDcWQgHj5SDUxJWsxJtf7c4XVEAuni",
+    clientId: "e7ZDcWQgHj5SDUxJWsxJtf7c4XVEAuni",
   };
